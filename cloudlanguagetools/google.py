704740a0edbb88403774adae95a7b7a15c4aaa73
luc wastiaux <luc.wastiaux@airpost.net>
2020-12-05 20:31:06 +0800

    added support for listing google voices

--- cloudlanguagetools/google.py
+++ cloudlanguagetools/google.py
@@ -1,5 +1,32 @@
 import google.cloud.texttospeech
 import cloudlanguagetools.service
+import cloudlanguagetools.constants
+
+def language_code_to_enum(language_code):
+    override_map = {
+        'cmn-TW': cloudlanguagetools.constants.Language.zh_TW,
+        'cmn-CN': cloudlanguagetools.constants.Language.zh_CN,
+        'yue-HK': cloudlanguagetools.constants.Language.zh_HK
+    }
+    if language_code in override_map:
+        return override_map[language_code]
+    language_enum_name = language_code.replace('-', '_')
+    return cloudlanguagetools.constants.Language[language_enum_name]
+
+class GoogleVoice(cloudlanguagetools.ttsvoice.TtsVoice):
+    def __init__(self, voice_data):
+        self.service = cloudlanguagetools.constants.Service.Google
+        self.name = voice_data.name
+        print(self.name)
+        gender_str = google.cloud.texttospeech.SsmlVoiceGender(voice_data.ssml_gender).name.lower().capitalize()
+        self.gender = cloudlanguagetools.constants.Gender[gender_str]
+        assert len(voice_data.language_codes) == 1
+        language_code = voice_data.language_codes[0]
+        self.language = language_code_to_enum(language_code)
+
+    def get_voice_id(self):
+        return self.name
+
 
 class GoogleService(cloudlanguagetools.service.Service):
     def __init__(self):
@@ -12,19 +39,11 @@ class GoogleService(cloudlanguagetools.service.Service):
         # Performs the list voices request
         voices = client.list_voices()
 
+        result = []
+
         for voice in voices.voices:
-            # Display the voice's name. Example: tpc-vocoded
-            print(f"Name: {voice.name}")
+            result.append(GoogleVoice(voice))
 
-            # Display the supported language codes for this voice. Example: "en-US"
-            for language_code in voice.language_codes:
-                print(f"Supported language: {language_code}")
+        return result
 
-            ssml_gender = google.cloud.texttospeech.SsmlVoiceGender(voice.ssml_gender)
-
-            # Display the SSML Voice Gender
-            print(f"SSML Voice Gender: {ssml_gender.name}")
-
-            # Display the natural sample rate hertz for this voice. Example: 24000
-            print(f"Natural Sample Rate Hertz: {voice.natural_sample_rate_hertz}\n")
         

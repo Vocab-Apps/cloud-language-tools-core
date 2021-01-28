a93d23f4a509f2e8062a75dbb16119b73599d5d6
luc wastiaux <luc.wastiaux@airpost.net>
2021-01-28 23:13:57 +0800

    simplify mandarin text

--- test_audio.py
+++ test_audio.py
@@ -74,7 +74,7 @@ class TestAudio(unittest.TestCase):
         self.verify_service_audio_language(source_text, Service.Google, AudioLanguage.zh_CN, 'zh-CN')
 
     def test_mandarin_azure(self):
-        source_text = '老人家'
+        source_text = '你好'
         self.verify_service_audio_language(source_text, Service.Azure, AudioLanguage.zh_CN, 'zh-CN') 
 
     def test_mandarin_watson(self):

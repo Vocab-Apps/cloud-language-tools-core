16edaa642b4338a78c2c8f6bd2acc54c31901cc6
luc wastiaux <luc.wastiaux@airpost.net>
2021-03-09 22:54:28 +0800

    decript tts keys inside start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+gpg --batch --yes --passphrase ${GPG_PASSPHRASE} --output tts_keys.sh  --decrypt tts_keys.sh.gpg
 source tts_keys.sh
 exec gunicorn -b :8042 --timeout 120 --access-logfile - --error-logfile - app:app

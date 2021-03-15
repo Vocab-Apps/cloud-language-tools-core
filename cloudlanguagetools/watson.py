93e0c8351c0b38e1d8a16b892c07b9ab65b09d77
luc wastiaux <luc.wastiaux@airpost.net>
2021-03-16 07:56:49 +0800

    fix error message

--- cloudlanguagetools/watson.py
+++ cloudlanguagetools/watson.py
@@ -140,7 +140,7 @@ class WatsonService(cloudlanguagetools.service.Service):
             return output_temp_file
 
         # otherwise, an error occured
-        error_message = f"Status code: {response.status_code} reason: {response.reason} voice: [{voice_name}] api key: [{api_key}]]"
+        error_message = f"Status code: {response.status_code} reason: {response.reason} voice: [{voice_name}]]"
         raise cloudlanguagetools.errors.RequestError(error_message)
 
 

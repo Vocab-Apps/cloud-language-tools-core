5a5c742f1ce51a01536ebc085f08f50322e5f38c
luc wastiaux <luc.wastiaux@airpost.net>
2021-03-30 13:00:11 +0800

    word not found

--- test_audio.py
+++ test_audio.py
@@ -155,6 +155,11 @@ class TestAudio(unittest.TestCase):
         source_text = 'absolument'
         self.verify_service_audio_language(source_text, Service.Forvo, AudioLanguage.fr_FR, 'fr-FR')
 
+    def test_french_forvo_not_found(self):
+        # pytest test_audio.py -k test_french_forvo_not_found
+        source_text = 'wordnotfound'
+        self.assertRaises(cloudlanguagetools.errors.NotFoundError, self.verify_service_audio_language, source_text, Service.Forvo, AudioLanguage.fr_FR, 'fr-FR')
+
 
     def test_azure_options(self):
         service = 'Azure'

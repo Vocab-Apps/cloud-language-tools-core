ec9006cae029c03801185ca8f644a7495965fd0b
luc wastiaux <luc.wastiaux@airpost.net>
2021-04-03 16:28:47 +0800

    combine api keys and patreon user id data

--- user_utils.py
+++ user_utils.py
@@ -1,5 +1,6 @@
 import pandas
 import logging
+import datetime
 
 import redisdb
 import airtable_utils
@@ -27,16 +28,27 @@ class UserUtils():
             }
             data.update(api_key_entry['key_data'])
             records.append(data)
-        return pandas.DataFrame(records)
+        data_df = pandas.DataFrame(records)
+        data_df['expiration_dt'] = pandas.to_datetime(data_df['expiration'], unit='s')
+        data_df['key_valid'] = data_df['expiration_dt'] > datetime.datetime.now()
+        data_df = data_df.rename(columns={'user_id': 'patreon_user_id'})
+
+        return data_df[['api_key', 'email', 'patreon_user_id', 'type', 'character_limit', 'expiration_dt', 'key_valid']]
 
     def get_patreon_users_df(self):
-        self.patreon_utils.get_patreon_user_ids()
-        return None
+        user_list = self.patreon_utils.get_patreon_user_ids()
+        user_list_df = pandas.DataFrame(user_list)
+        user_list_df = user_list_df.rename(columns={'user_id': 'patreon_user_id'})
+        return user_list_df
 
     def build_user_data(self):
         api_key_list_df = self.get_api_key_list_df()
         print(api_key_list_df)
         patreon_user_df = self.get_patreon_users_df()
+        print(patreon_user_df)
+
+        combined_df = pandas.merge(api_key_list_df, patreon_user_df, how='left', on='patreon_user_id')
+        print(combined_df)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', 
@@ -44,5 +56,5 @@ if __name__ == '__main__':
                         level=logging.INFO)
     user_utils = UserUtils()
 
-    # user_utils.build_user_data()
-    user_utils.get_patreon_users_df()
+    user_utils.build_user_data()
+    # user_utils.get_patreon_users_df()

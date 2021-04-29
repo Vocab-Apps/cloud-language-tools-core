0745ddc521593b5cadbd35149f0a8389f989d019
luc wastiaux <luc.wastiaux@airpost.net>
2021-04-29 13:12:23 +0800

    disable trial_user_utils

--- trial_user_utils.py
+++ trial_user_utils.py
@@ -202,6 +202,8 @@ class TrialUserUtils():
 
 
 def main():
+    raise Exception('should not be used anymore, replaced by user_utils.py')
+
     logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', 
                         datefmt='%Y%m%d-%H:%M:%S',
                         level=logging.INFO)

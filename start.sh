c80555d647ccbdc88f6dfd3df1fd739d0ed86231
luc wastiaux <luc.wastiaux@airpost.net>
2021-05-19 18:38:40 +0800

    fix shell syntax

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ gpg --batch --yes --passphrase ${GPG_PASSPHRASE} --output ${CWD}/convertkit.sh  
 gpg --batch --yes --passphrase ${GPG_PASSPHRASE} --output ${CWD}/airtable.sh  --decrypt airtable.sh.gpg
 gpg --batch --yes --passphrase ${GPG_PASSPHRASE} --output ${CWD}/digitalocean_spaces.sh  --decrypt digitalocean_spaces.sh.gpg
 gpg --batch --yes --passphrase ${GPG_PASSPHRASE} --output ${CWD}/patreon_prod_digitalocean.sh  --decrypt patreon_prod_digitalocean.sh.gpg
-if [ -v $RUN_SCHEDULED_TASKS ]
+if [ -n "$RUN_SCHEDULED_TASKS" ]
 then
 . ${CWD}/convertkit.sh
 . ${CWD}/airtable.sh
 . ${CWD}/digitalocean_spaces.sh
 . ${CWD}/patreon_prod_digitalocean.sh
-python scheduled_tasks.py
+python3 scheduled_tasks.py
 else
 . ${CWD}/tts_keys.sh
 . ${CWD}/convertkit.sh

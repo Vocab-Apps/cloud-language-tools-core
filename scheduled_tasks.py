7ab7311029c21507e6445c4e995ea99cc00488c1
luc wastiaux <luc.wastiaux@airpost.net>
2021-05-20 21:15:49 +0800

    disable scp as outgoing ssh is disable on digital ocean

--- scheduled_tasks.py
+++ scheduled_tasks.py
@@ -29,17 +29,6 @@ def backup_redis_db():
     client.put_object(Body=str(json.dumps(full_db_dump)), Bucket=bucket_name, Key=file_name)
     logging.info(f'wrote {file_name} to {bucket_name}')
 
-    # write to disk
-    f = open(file_name, 'w')
-    f.write(json.dumps(full_db_dump))
-    f.close()
-    
-    # scp to rsync.net
-    scp_commandline = f'scp -i ssh_id_rsync_redis_backup {file_name} {scp_username}@{scp_hostname}:backup/digitalocean_redis/'
-    logging.info(f'scp commandline: [{scp_commandline}]')
-    os.system(scp_commandline)
-
-
 
 def update_airtable():
     logging.info('updating airtable')

c62e3f6f1a3e35e46b71f8e707558ffc6aaf1a43
luc wastiaux <luc.wastiaux@airpost.net>
2021-06-03 22:17:12 +0800

    attempt to get customer

--- getcheddar_utils.py
+++ getcheddar_utils.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import requests
+import urllib.parse
 import xml.dom.minidom
 
 PRODUCT_CODE='LANGUAGE_TOOLS'
@@ -13,15 +14,32 @@ class GetCheddarUtils():
     
 
     def report_customer_usage(self, customer_code):
-        url = f'https://getcheddar.com/xml/customers/add-item-quantity/productCode/{PRODUCT_CODE}/code/{customer_code}/itemCode/{TRACKED_ITEM_CODE}'
+        customer_code_encoded = urllib.parse.quote(customer_code)
+        url = f'https://getcheddar.com/xml/customers/add-item-quantity/productCode/{PRODUCT_CODE}/code/{customer_code_encoded}/itemCode/{TRACKED_ITEM_CODE}'
         print(url)
-        params = {'quantity': 0.012345}
+        params = {'quantity': 123.45}
         response = requests.post(url, auth=(self.user, self.api_key), data=params)
         if response.status_code == 200:
             # success
             dom = xml.dom.minidom.parseString(response.content)
             pretty_xml_as_string = dom.toprettyxml(newl='')
             print(pretty_xml_as_string)
+        else:
+            print(response.content)
+
+    def get_customer(self, customer_code):
+        # /customers/get/productCode/MY_PRODUCT_CODE/code/MY_CUSTOMER_CODE
+        customer_code_encoded = urllib.parse.quote(customer_code)
+        url = f'https://getcheddar.com/xml/customers/get/productCode/{PRODUCT_CODE}/code/{customer_code_encoded}'
+        print(url)
+        response = requests.get(url, auth=(self.user, self.api_key))
+        if response.status_code == 200:
+            # success
+            dom = xml.dom.minidom.parseString(response.content)
+            pretty_xml_as_string = dom.toprettyxml(newl='')
+            print(pretty_xml_as_string)
+        else:
+            print(response.content)
 
 if __name__ == '__main__':
 
@@ -30,5 +48,6 @@ if __name__ == '__main__':
                         level=logging.INFO)
 
     cheddar_utils = GetCheddarUtils()
-    customer_code = 'no1@spam.com'
+    customer_code = 'languagetools+customer2@mailc.net '
     cheddar_utils.report_customer_usage(customer_code)
+    # cheddar_utils.get_customer(customer_code)

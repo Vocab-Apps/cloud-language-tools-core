cf27b2d6563ce04e946a2d889a582c6209ceefd1
luc wastiaux <luc.wastiaux@airpost.net>
2021-06-09 20:52:13 +0800

    update translation value

--- test_translation.py
+++ test_translation.py
@@ -79,7 +79,7 @@ class TestTranslation(unittest.TestCase):
         self.translate_text(Service.Azure, '送外卖的人', Language.zh_cn, Language.en, 'The person who sent the takeaway')
         self.translate_text(Service.Google, '中国有很多外国人', Language.zh_cn, Language.en, 'There are many foreigners in China')
         self.translate_text(Service.Azure, '成本很低', Language.zh_cn, Language.fr, 'Le coût est très faible')
-        self.translate_text(Service.Google, '换登机牌', Language.zh_cn, Language.fr, "Changer de carte d'embarquement")
+        self.translate_text(Service.Google, '换登机牌', Language.zh_cn, Language.fr, "Changer la carte d'embarquement")
         self.translate_text(Service.Amazon, '换登机牌', Language.zh_cn, Language.fr, "Modifier la carte d'embarquement")
 
     def test_translate_chinese_watson(self):

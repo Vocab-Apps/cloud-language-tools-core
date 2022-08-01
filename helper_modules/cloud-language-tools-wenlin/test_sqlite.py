20ddc0b22a16940366d8b706e779754e842d5a0d
luc wastiaux <luc.wastiaux@airpost.net>
2022-08-01 09:58:16 +0800

    verify we can find entries by querying

--- helper_modules/cloud-language-tools-wenlin/test_sqlite.py
+++ helper_modules/cloud-language-tools-wenlin/test_sqlite.py
@@ -1,6 +1,8 @@
 import unittest
 import clt_wenlin
 import tempfile
+import sqlite3
+import json
 
 
 class TestWenlinSqlite(unittest.TestCase):
@@ -9,5 +11,53 @@ class TestWenlinSqlite(unittest.TestCase):
         sqlite_tempfile = tempfile.NamedTemporaryFile(suffix='.db')
 
         clt_wenlin.create_sqlite_file(dictionary_filepath, sqlite_tempfile.name)
+
+        # try to query the file
+        connection = sqlite3.connect(sqlite_tempfile.name)
+        cur = connection.cursor()
+
+        query = """SELECT entry FROM words WHERE simplified='啊'"""
+        results = []
+        for row in cur.execute(query):
+            results.append(row[0])
+
+        self.assertEqual(len(results), 5)
+        entry_json_str = results[0]
+        entry_dict = json.loads(entry_json_str)
+
+        expected_entry_dict = {'parts_of_speech': [
+                                    {'definitions': [
+                                                {'definition': 'used as phrase suffix'},
+                                                {'definition': 'in enumeration',
+                                                'example_chinese': '钱∼, 书∼, 表∼, 我都丢了。',
+                                                'example_pinyin': 'Qián ∼, shū ∼, biǎo '
+                                                                    '∼, wǒ dōu diū le.',
+                                                'example_translation': 'Money, books, '
+                                                                        'watch, I lost '
+                                                                        'everything.'},
+                                                {'definition': 'in direct address and '
+                                                                'exclamation',
+                                                'example_chinese': '老王∼, 这可不行∼!',
+                                                'example_pinyin': 'Lǎo Wáng ∼, zhè kě '
+                                                                    'bùxíng ∼!',
+                                                'example_translation': 'Wang, this '
+                                                                        "won't do!"},
+                                                {'definition': 'indicating '
+                                                                'obviousness/impatience',
+                                                'example_chinese': '来∼!',
+                                                'example_pinyin': 'Lái ∼!',
+                                                'example_translation': 'Come on!'},
+                                                {'definition': 'for confirmation',
+                                                'example_chinese': '你不来∼?',
+                                                'example_pinyin': 'Nǐ bù lái ∼?',
+                                                'example_translation': "So you're not "
+                                                                        'coming?'}],
+                                            'part_of_speech': 'm.p.'}
+                                        ],
+        'simplified': '啊',
+        'traditional': '啊'}        
+
+        self.maxDiff = None
+        self.assertEqual(entry_dict, expected_entry_dict)
 
 

b243e8d3b72a1e4ce44bdbc0fa337e29abd15284
luc wastiaux <luc.wastiaux@airpost.net>
2022-08-01 16:32:38 +0800

    sort list

--- cloudlanguagetools/wenlin.py
+++ cloudlanguagetools/wenlin.py
@@ -116,5 +116,6 @@ class WenlinService(cloudlanguagetools.service.Service):
         ]:
             # retain unique
             result = list(set(result))
+            result.sort()
 
         return result

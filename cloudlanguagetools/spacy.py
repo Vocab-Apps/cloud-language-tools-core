624920b6de3d19eb0b39f6b39272fa91aeeea801
luc wastiaux <luc.wastiaux@airpost.net>
2022-09-19 09:31:38 +0800

    use remote spacy API

--- cloudlanguagetools/spacy.py
+++ cloudlanguagetools/spacy.py
@@ -1,11 +1,13 @@
-import spacy
 import logging
+import requests
 import os
 
 import cloudlanguagetools.service
 import cloudlanguagetools.constants
 import cloudlanguagetools.languages
 import cloudlanguagetools.tokenization
+
+logger = logging.getLogger(__name__)
 
 class SpacyTokenization(cloudlanguagetools.tokenization.Tokenization):
     def __init__(self, language, model_name, variant=None):
@@ -27,72 +29,34 @@ class SpacyTokenization(cloudlanguagetools.tokenization.Tokenization):
         }
 
 class SpacyService(cloudlanguagetools.service.Service):
+    BASE_URL = 'http://spacy-api.vocab.ai'
+
     def __init__(self):
-        self.nlp_engine_cache = {}
-        os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-    def preload_data(self):
-        # pre-load nlp engines (to test ram utilization)
-        for tokenization_option in self.get_tokenization_options():
-            model_name = tokenization_option.get_tokenization_key()['model_name']
-            nlp_engine = self.get_nlp_engine(model_name)
-        
-
-    def build_nlp_engine(self, model_name):
-        import spacy.lang.zh
-        if model_name == 'chinese_char':
-            return spacy.lang.zh.Chinese()
-        if model_name == 'chinese_jieba':
-            return spacy.lang.zh.Chinese.from_config({"nlp": {"tokenizer": {"segmenter": "jieba"}}})
-        if model_name == 'chinese_pkuseg':
-            return spacy.lang.zh.Chinese.from_config({"nlp": {"tokenizer": {"segmenter": "pkuseg"}}})
-        return spacy.load(model_name)        
-
-    def get_nlp_engine(self, model_name):
-        if model_name in self.nlp_engine_cache:
-            return self.nlp_engine_cache[model_name]
-
-        logging.info(f'loading model {model_name}')
-        nlp_engine = self.build_nlp_engine(model_name)
-        self.nlp_engine_cache[model_name] = nlp_engine
-        return nlp_engine
+        self.BASE_URL = os.environ.get('SPACY_URL_OVERRIDE', self.BASE_URL)
 
     def get_tokenization(self, text, tokenization_key):
         model_name = tokenization_key['model_name']
-        nlp_engine = self.get_nlp_engine(model_name)
 
-        doc = nlp_engine(text)
-        result = []
-        for token in doc:
-            lemma = token.lemma_
-            if len(lemma) == 0:
-                lemma = str(token)
-            pos_description = spacy.explain(token.tag_)
-            entry = {
-                'token': str(token),
-                'lemma': lemma,
-                'can_translate': token.is_alpha,
-                'can_transliterate': token.is_alpha
-            }
-            if pos_description != None:
-                entry['pos_description'] = pos_description
-            result.append(entry)
-        return result
+        response = requests.post(self.BASE_URL + '/v1/tokenize', data={'language': model_name, 'text': text}, timeout=cloudlanguagetools.constants.RequestTimeout)
+        response_data = response.json()        
+
+        if response.status_code == 200:
+            return response_data
 
         # raise exception
-        raise cloudlanguagetools.errors.RequestError(f'unsupported tokenization mode: {mode.name}')
+        raise cloudlanguagetools.errors.RequestError(f'could not generate tokenization model_name {model_name} text: {text}: {response}')
 
 
     def get_tokenization_options(self):
         result = [
-            SpacyTokenization(cloudlanguagetools.languages.Language.en, 'en_core_web_trf'),
-            SpacyTokenization(cloudlanguagetools.languages.Language.fr, 'fr_dep_news_trf'),
-            SpacyTokenization(cloudlanguagetools.languages.Language.ja, 'ja_core_news_lg'),
-            SpacyTokenization(cloudlanguagetools.languages.Language.de, 'de_dep_news_trf'),
-            SpacyTokenization(cloudlanguagetools.languages.Language.es, 'es_dep_news_trf'),
-            SpacyTokenization(cloudlanguagetools.languages.Language.ru, 'ru_core_news_lg'),
-            SpacyTokenization(cloudlanguagetools.languages.Language.pl, 'pl_core_news_lg'),
-            SpacyTokenization(cloudlanguagetools.languages.Language.it, 'it_core_news_lg'),
+            SpacyTokenization(cloudlanguagetools.languages.Language.en, 'en'),
+            SpacyTokenization(cloudlanguagetools.languages.Language.fr, 'fr'),
+            SpacyTokenization(cloudlanguagetools.languages.Language.ja, 'ja'),
+            SpacyTokenization(cloudlanguagetools.languages.Language.de, 'de'),
+            SpacyTokenization(cloudlanguagetools.languages.Language.es, 'es'),
+            SpacyTokenization(cloudlanguagetools.languages.Language.ru, 'ru'),
+            SpacyTokenization(cloudlanguagetools.languages.Language.pl, 'pl'),
+            SpacyTokenization(cloudlanguagetools.languages.Language.it, 'it'),
 
         ]
         chinese_language_list = [
@@ -103,8 +67,8 @@ class SpacyService(cloudlanguagetools.service.Service):
         for language in chinese_language_list:
             # chinese variants
             result.extend([
-                SpacyTokenization(language, 'chinese_char', 'Characters'),
-                SpacyTokenization(language, 'chinese_jieba', 'Jieba (words)'),
-                SpacyTokenization(language, 'chinese_jieba', 'PKUSeg (words)')
+                SpacyTokenization(language, 'zh_char', 'Characters'),
+                SpacyTokenization(language, 'zh_jieba', 'Jieba (words)'),
+                SpacyTokenization(language, 'zh_pkuseg', 'PKUSeg (words)')
             ])
         return result

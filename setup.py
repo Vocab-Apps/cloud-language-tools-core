af22b15b3cdaf03c08f51aa7b6f252485297271c
Luc W <lucw@airpost.net>
2023-01-17 08:48:58 +0800

    upgraded version to 4.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.install import install
 # twine upload dist/*
 
 setup(name='cloudlanguagetools',
-      version='4.4',
+      version='4.5',
       description='Interface with various cloud APIs for language processing such as translation, text to speech',
       long_description=open('README.rst', encoding='utf-8').read(),
       url='https://github.com/Language-Tools/cloud-language-tools-core',

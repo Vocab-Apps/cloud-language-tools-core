5501791f9166e8a45fd48702fc3b61de129e3b05
Luc W <lucw@airpost.net>
2024-01-13 19:25:06 +0800

    update naver voice list

--- utils/parse_naver_voices.py
+++ utils/parse_naver_voices.py
@@ -1,70 +1,73 @@
 import re
 
+# voice list is here: https://api.ncloud-docs.com/docs/en/ai-naver-clovavoice-ttspremium
+# python utils/parse_naver_voices.py | sort  > naver_voice_list
+
 naver_voice_list = """
-    nara : Ara: Korean, female voice
-    nara_call : Ara (agent): Korean, female voice
-    nminyoung : Minyoung: Korean, female voice
-    nyejin : Yejin: Korean, female voice
-    mijin : Mijin: Korean, female voice
-    jinho : Jinho: Korean, male voice
-    clara : Clara: English, female voice
-    matt : Matt: English, male voice
-    shinji : Shinji: Japanese, male voice
-    meimei : Meimei: Chinese, female voice
-    liangliang : Liangliang: Chinese, male voice
+    nara: Ara: Korean, female voice
+    nara_call: Ara (agent): Korean, female voice
+    nminyoung: Minyoung: Korean, female voice
+    nyejin: Yejin: Korean, female voice
+    mijin: Mijin: Korean, female voice
+    jinho: Jinho: Korean, male voice
+    clara: Clara: English, female voice
+    matt: Matt: English, male voice
+    shinji: Shinji: Japanese, male voice
+    meimei: Meimei: Chinese, female voice
+    liangliang: Liangliang: Chinese, male voice
     jose: Jose: Spanish, male voice
     carmen: Carmen: Spanish, female voice
     nminsang: Minsang: Korean, male voice
-    nsinu: Shinwoo: Korean, male voice
+    nsinu: Sinwoo: Korean, male voice
     nhajun: Hajun: Korean, child voice (male)
-    ndain: Dain: Korean, child tone (female)
-    njiyun : Jiyun: Korean, female voice
-    nsujin : Sujin: Korean, female voice
-    njinho : Jinho: Korean, male voice
-    njihun : Jihun: Korean, male voice
-    njooahn : Jooan: Korean, male voice
-    nseonghoon : Seonghun: Korean, male voice
-    njihwan : Jihwan: Korean, male voice
-    nsiyoon : Siyoon: Korean, male voice
-    ngaram : Garam: Korean, children's voice (female)
-    ntomoko : Tomoko: Japanese, female voice
-    nnaomi : Naomi: Japanese, female voice
-    dnaomi_joyful : Naomi (joy): Japanese, female voice
-    dnaomi_formal : Naomi (news): Japanese, female voice
-    driko : Riko: Japanese, female voice
-    deriko : Eriko: Japanese, female voice
-    nsayuri : Sayuri: Japanese, female voice
-    ngoeun : Goeun: Korean, female voice
-    neunyoung : Eunyoung : Korean, female voice
-    nsunkyung : Seonkyung: Korean, female voice
-    nyujin : Yujin: Korean, female voice
-    ntaejin : Taejin: Korean, male voice
-    nyoungil : Youngil: Korean, male voice
-    nseungpyo : Seungpyo: Korean, male voice
-    nwontak : Wontak: Korean, male voice
-    dara_ang : Ara (angry): Korean, female voice
-    nsunhee : Seonhee: Korean, female voice
-    nminseo : Minseo: Korean, female voice
+    ndain: Dain: Korean, child voice (female)
+    njiyun: Jiyun: Korean, female voice
+    nsujin: Sujin: Korean, female voice
+    njinho: Jinho: Korean, male voice
+    njihun: Jihun: Korean, male voice
+    njooahn: Jooahn: Korean, male voice
+    nseonghoon: Seonghoon: Korean, male voice
+    njihwan: Jihwan: Korean, male voice
+    nsiyoon: Siyoon: Korean, male voice
+    ngaram: Garam: Korean, child voice (female)
+    ntomoko: Tomoko: Japanese, female voice
+    nnaomi: Naomi: Japanese, female voice
+    dnaomi_joyful: Naomi (joyful): Japanese, female voice
+    dnaomi_formal: Naomi (news): Japanese, female voice
+    driko: Riko: Japanese, female voice
+    deriko: Eriko: Japanese, female voice
+    nsayuri: Sayuri: Japanese, female voice
+    ngoeun: Goeun: Korean, female voice
+    neunyoung: Eunyoung: Korean, female voice
+    nsunkyung: Sunkyung: Korean, female voice
+    nyujin: Yujin: Korean, female voice
+    ntaejin: Taejin: Korean, male voice
+    nyoungil: Youngil: Korean, male voice
+    nseungpyo: Seungpyo: Korean, male voice
+    nwontak: Wontak: Korean, male voice
+    dara_ang: Ara (angry): Korean, female voice
+    nsunhee: Sunhee: Korean, female voice
+    nminseo: Minseo: Korean, female voice
     njiwon: Jiwon: Korean, female voice
     nbora: Bora: Korean, female voice
     njonghyun: Jonghyun: Korean, male voice
-    njoonyoung: Junyoung: Korean, male voice
+    njoonyoung: Joonyoung: Korean, male voice
     njaewook: Jaewook: Korean, male voice
     danna: Anna: English, female voice
-    djoey : Joy: English, female voice
+    djoey: Joey: English, female voice
     dhajime: Hajime: Japanese, male voice
     ddaiki: Daiki: Japanese, male voice
     dayumu: Ayumu: Japanese, male voice
     dmio: Mio: Japanese, female voice
-    chiahua: Chahwa: Taiwanese, female voice
-    kuanlin: Guanlin: Taiwanese, male voice
+    chiahua: Chiahua: Taiwanese, female voice
+    kuanlin: Kuanlin: Taiwanese, male voice
     nes_c_hyeri: Hyeri: Korean, female voice
-    nes_c_sohyun: Sohyeon: Korean, female voice
+    nes_c_sohyun: Sohyun: Korean, female voice
     nes_c_mikyung: Mikyung: Korean, female voice
     nes_c_kihyo: Kihyo: Korean, male voice
-    ntiffany: Giseo: Korean, female voice
+    ntiffany: Kiseo: Korean, female voice
     napple: Neulbom: Korean, female voice
-    njangj: Dream: Korean, female voice
+    njangj: Deurim: Korean, female voice
     noyj: Bomdal: Korean, female voice
     neunseo: Eunseo: Korean, female voice
     nheera: Heera: Korean, female voice
@@ -72,22 +75,43 @@ naver_voice_list = """
     nnarae: Narae: Korean, female voice
     nyeji: Yeji: Korean, female voice
     nyuna: Yuna: Korean, female voice
-    nkyunglee: Kyungri: Korean, female voice
+    nkyunglee: Kyunglee: Korean, female voice
     nminjeong: Minjeong: Korean, female voice
-    nihyun: Lee Hyun: Korean, female voice
+    nihyun: Leehyeon: Korean, female voice
     nraewon: Raewon: Korean, male voice
-    nkyuwon: Gyuwon: Korean, male voice
+    nkyuwon: Kyuwon: Korean, male voice
     nkitae: Kitae: Korean, male voice
     neunwoo: Eunwoo: Korean, male voice
-    nkyungtae: Gyeongtae: Korean, male voice
+    nkyungtae: Kyungtae: Korean, male voice
     nwoosik: Woosik: Korean, male voice
-    vara: Ara: Korean, female voice
-    vmikyung: Mikyung: Korean, female voice
-    vdain: Dain: Korean, female voice
-    vyuna: Yuna: Korean, female voice
-    vhyeri : Hyeri: Korean, female voice
-    dara-danna: Ara&Anna: Korean + English (US), female voice
+    vara: Ara (pro): Korean, female voice
+    vmikyung: Mikyung (pro): Korean, female voice
+    vdain: Dain (pro): Korean, female voice
+    vyuna: Yuna (pro): Korean, female voice
+    vhyeri: Hyeri (pro): Korean, female voice
+    dara-danna: Ara & Anna: Korean + English (US), female voice
     dsinu-matt: Shinwoo & Matt: Korean + English (US), male voice
+    nsabina: Witch Sabina: Korean, female voice
+    nmammon: Demon Mammon: Korean, male voice
+    nmeow: Kitty: Korean, child voice (female)
+    nwoof: Doggy: Korean, child voice (male)
+    nreview: Park Leebyu: Korean, male voice
+    nyounghwa: Jung Younghwa: Korean, female voice
+    nmovie: Choi Moobi: Korean, male voice
+    nsangdo: Sangdo: Korean, male voice
+    nshasha: Shasha: Korean, female voice
+    nian: Ian: Korean, male voice
+    ndonghyun: Donghyun: Korean, male voice
+    vian: Ian (pro): Korean, male voice
+    vdonghyun: Donghyun (pro): Korean, male voice
+    dsayuri: Sayuri: Japanese, female voice
+    dtomoko: Tomoko: Japanese, female voice
+    dnaomi: Naomi: Japanese, female voice
+    vgoeun: Goeun (pro): Korean, female voice
+    vdaeseong: Daeseong (pro): Korean, male voice
+    ngyeongjun: Gyeongjun: Korean, male voice
+    ndaeseong: Daeseong: Korean, male voice
+    njonghyeok: Jonghyeok: Korean, male voice
 """
 
 LANGUAGE_MAP = {

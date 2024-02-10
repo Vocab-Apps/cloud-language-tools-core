46840cbb15b41b2d814efa912926be54fc899a8c
Luc W <lucw@airpost.net>
2024-02-10 17:43:48 +0800

    upgrade to clt_requirements 1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='cloudlanguagetools',
       packages=['cloudlanguagetools'],
       install_requires=[
           'clt_wenlin>=1.0',
-          'clt_requirements>=1.1',
+          'clt_requirements>=1.3',
           'pinyin_jyutping>=0.9',
       ],
       )

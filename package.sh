dbb7bdb89aa80e8da477572572fb5b71ebdb601b
Luc W <lucw@airpost.net>
2024-03-07 06:49:35 +0800

    build and push image

--- package.sh
+++ package.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# exit if parameter is not passed in
+if [ -z "$1" ]; then
+    echo "Please provide a version number"
+    exit 1
+fi
+
 VERSION_NUMBER=$1 # for example 0.1
 GIT_TAG=v${VERSION_NUMBER}
 
@@ -12,3 +20,8 @@ git push origin ${GIT_TAG}
 source ~/secrets/python/twine.sh
 python setup.py sdist
 twine upload dist/*
+
+# build docker image
+export DOCKER_BUILDKIT=1
+docker build -t lucwastiaux/cloud-language-tools-core:${VERSION_NUMBER} --build-arg CLT_CORE_VERSION=${VERSION_NUMBER} -f Dockerfile.clt_core .
+docker push lucwastiaux/cloud-language-tools-core:${VERSION_NUMBER}

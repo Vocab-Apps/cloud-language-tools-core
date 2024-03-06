7c1ac3f52e8f54fdeba16a51bc09601f96778872
Luc W <lucw@airpost.net>
2024-03-07 06:10:59 +0800

    read requirements from requirements.txt

--- helper_modules/cloud-language-tools-requirements/setup.py
+++ helper_modules/cloud-language-tools-requirements/setup.py
@@ -4,34 +4,20 @@ from setuptools import setup
 # python setup.py sdist
 # twine upload dist/*
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='clt_requirements',
-      version='1.3',
+      version='1.4',
       description='Helper module for Cloud Language Tools, additional dependencies',
       url='https://github.com/Language-Tools/cloud-language-tools-core',
       author='Luc',
       author_email='languagetools@mailc.net',
       classifiers=[
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Text Processing :: Linguistic',
       ],      
       license='GPL',
       packages=['clt_requirements'],
-      install_requires=[
-          'azure-cognitiveservices-speech',
-          'requests',
-          'google-cloud-texttospeech',
-          'google-cloud-translate',
-          'boto3',
-          'epitran==1.22',
-          'pythainlp[thai2rom,ipa]',
-          'python-crfsuite',
-          'jieba',
-          'cryptography',
-          'pydub',
-          'openai>=1.7.2',
-          'pydantic',
-          'StrEnum',
-          'cachetools'
-      ],
+      install_requires=requirements,
       )

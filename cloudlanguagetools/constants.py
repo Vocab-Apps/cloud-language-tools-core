d22582bf587d4357a01d54bfd1b62123fe1ffc17
Luc W <lucw@airpost.net>
2024-03-07 06:34:23 +0800

    fix clt_requirements, fix strenum for python 3.11

--- cloudlanguagetools/constants.py
+++ cloudlanguagetools/constants.py
@@ -1,5 +1,4 @@
 import enum
-from strenum import StrEnum
 
 # usage/account related constants 
 # ===============================
@@ -54,7 +53,7 @@ ReadTimeout = 3 # 3 seconds read timeout
 
 TTLCacheTimeout = 86400 # 24 hours
 
-class Service(StrEnum):
+class Service(enum.StrEnum):
     Azure = 'Azure'
     Google = 'Google'
     MandarinCantonese = 'MandarinCantonese'
@@ -78,7 +77,7 @@ class Service(StrEnum):
     TestServiceA = 'TestServiceA'
     TestServiceB = 'TestServiceB'
 
-class Gender(StrEnum):
+class Gender(enum.StrEnum):
     Male = 'Male'
     Female = 'Female'
     Any = 'Any'

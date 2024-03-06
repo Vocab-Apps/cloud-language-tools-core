13229d3920549ec370275cf24f3ffa3115082e04
Luc W <lucw@airpost.net>
2024-03-07 06:41:44 +0800

    add build scripts for clt_requirements

---
+++ build_clt_requirements_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# exit if an argument is not passed
+if [ -z "$1" ]; then
+    echo "Please provide a version number"
+    exit 1
+fi
+
+CLT_REQUIREMENTS_VERSION=$1 # for example 0.1
+
+export DOCKER_BUILDKIT=1
+DOCKER_IMAGE=lucwastiaux/clt-requirements
+docker build -t ${DOCKER_IMAGE}:${CLT_REQUIREMENTS_VERSION} --build-arg CLT_REQUIREMENTS_VERSION=${CLT_REQUIREMENTS_VERSION} -f Dockerfile.clt_requirements .
+docker push ${DOCKER_IMAGE}:${CLT_REQUIREMENTS_VERSION}

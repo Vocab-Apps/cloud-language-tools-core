3a39786a1f3b1ba82d9578f0d6fca9622efd4d4c
Luc W <lucw@airpost.net>
2024-03-09 18:05:40 +0800

    run docker test

--- package.sh
+++ package.sh
@@ -17,14 +17,14 @@ git tag -a ${GIT_TAG} -m "version ${GIT_TAG}"
 git push origin ${GIT_TAG}
 
 # build python module , upload to pypi
-# get twice user/password
+# get twine user/password
 source ~/secrets/python/twine.sh
 python setup.py sdist
 twine upload dist/cloudlanguagetools-${VERSION_NUMBER}.tar.gz
 
-# wait for package to be updated on pypi
-# python utils/wait_pypi_version_update.py --package cloudlanguagetools --version ${VERSION_NUMBER}
-
-# get script directory
+# build clt-core docker image
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ${SCRIPT_DIR}/build_clt_core_docker.sh ${VERSION_NUMBER}
+
+# run test using docker
+${SCRIPT_DIR}/run_clt_docker.test.sh ${VERSION_NUMBER}

d63f85465d5379c42bb9d1290c083e60d08046a8
Luc W <lucw@airpost.net>
2024-03-09 17:27:38 +0800

    working distributed testing

--- run_clt_docker.test.sh
+++ run_clt_docker.test.sh
@@ -14,4 +14,6 @@ VERSION_NUMBER=$1 # for example 0.1
 export DOCKER_BUILDKIT=1
 DOCKER_IMAGE=lucwastiaux/cloud-language-tools-core-test
 docker build -t ${DOCKER_IMAGE}:${VERSION_NUMBER} --build-arg CLT_CORE_VERSION=${VERSION_NUMBER} -f Dockerfile.test .
-docker run --env CLOUDLANGUAGETOOLS_CORE_KEY=${CLOUDLANGUAGETOOLS_CORE_KEY} --rm -it ${DOCKER_IMAGE}:${VERSION_NUMBER} pytest -n10 --dist=loadfile tests
+# https://pypi.org/project/pytest-xdist/2.5.0/#features
+# --dist load (default): Sends pending tests to any worker that is available, without any guaranteed order.
+docker run --env CLOUDLANGUAGETOOLS_CORE_KEY=${CLOUDLANGUAGETOOLS_CORE_KEY} --rm -it ${DOCKER_IMAGE}:${VERSION_NUMBER} pytest -n4 --dist=load tests

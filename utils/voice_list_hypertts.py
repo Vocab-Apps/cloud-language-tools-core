b52ee03331c6ff650eb8cc90b6155afd17fa9f33
Luc W <lucw@airpost.net>
2024-06-18 06:15:21 +0800

    work in progress (generate list of multilingual voices in hypertts)

--- utils/voice_list_hypertts.py
+++ utils/voice_list_hypertts.py
@@ -26,7 +26,19 @@ def get_manager():
 
 def process_voice(voice: cloudlanguagetools.ttsvoice.TtsVoice_v3):
     logger.info(f'processing voice {voice}')
-    return databind.json.dump(voice, cloudlanguagetools.ttsvoice.TtsVoice_v3)
+    return f"""
+        voice.TtsVoice_v3(
+            name='{voice.name}',
+            voice_key={voice.voice_key},
+            options={voice.options},
+            service='{voice.service.name}',
+            gender=constants.Gender.{voice.gender.name},
+            audio_languages=[
+                {', '.join([f'languages.AudioLanguage.{audio_lang.name}' for audio_lang in voice.audio_languages])}
+            ],
+            service_fee=constants.ServiceFee.{voice.service_fee.name}
+        )
+"""
 
 def get_voice_list_hypertts():
     # for hypertts
@@ -34,11 +46,21 @@ def get_voice_list_hypertts():
     tts_voice_list:  List[cloudlanguagetools.ttsvoice.TtsVoice_v3] = manager.get_tts_voice_list_v3()
     # sort by voice_description
     tts_voice_list.sort(key=lambda voice: (voice.service, voice.name))
-    tts_voice_list = [process_voice(voice) for voice in tts_voice_list]
-    output_filename = 'temp_data_files/hypertts_voicelist.json'
+    voice_list_str_array = [process_voice(voice) for voice in tts_voice_list]
+    output_filename = 'temp_data_files/hypertts_voicelist.py'
     
     with open(output_filename, 'w', encoding='utf8') as f:
-        json.dump(tts_voice_list, f, ensure_ascii=False, indent=4)
+        f.write("""
+import sys
+
+constants = __import__('constants', globals(), locals(), [], sys._addon_import_level_services)
+languages = __import__('languages', globals(), locals(), [], sys._addon_import_level_services)
+voice = __import__('voice', globals(), locals(), [], sys._addon_import_level_services)                
+
+VOICE_LIST = [
+        """)
+        f.write(', \n'.join(voice_list_str_array))
+        f.write("""]""")
 
     print(f'wrote {output_filename}')    
 

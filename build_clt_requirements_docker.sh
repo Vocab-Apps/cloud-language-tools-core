038c17a4bfc00b67a5e24a12d3e052ea7469a458
Your Name <you@example.com>
2025-03-02 05:51:42 +0800

    use new docker space

--- build_clt_requirements_docker.sh
+++ build_clt_requirements_docker.sh
@@ -9,6 +9,6 @@ fi
 CLT_REQUIREMENTS_VERSION=$1 # for example 0.1
 
 export DOCKER_BUILDKIT=1
-DOCKER_IMAGE=lucwastiaux/clt-requirements
+DOCKER_IMAGE=vocabai/clt-requirements
 docker build -t ${DOCKER_IMAGE}:${CLT_REQUIREMENTS_VERSION} --build-arg CLT_REQUIREMENTS_VERSION=${CLT_REQUIREMENTS_VERSION} -f Dockerfile.clt_requirements .
 docker push ${DOCKER_IMAGE}:${CLT_REQUIREMENTS_VERSION}

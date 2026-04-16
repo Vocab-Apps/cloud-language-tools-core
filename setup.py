8007d00055570cf087bbbb04835ebdea9b5c0870
Luc <luc@vocab.ai>
2026-04-17 05:47:30 +0800

    Bump clt_requirements to >=2.3

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(name='cloudlanguagetools',
       license='GPL',
       packages=['cloudlanguagetools'],
       install_requires=[
-          'clt_requirements>=2.2',
+          'clt_requirements>=2.3',
       ],
       )
